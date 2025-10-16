f1661857794164f07dd788e4b64656ac5f24f796
PDJ-N <qkreo321@gmail.com>
2025-10-17 03:22:40 +0900

    chore(init): React  프로젝트 초기 세팅

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'capstone-fe',
+      short_name: 'Running Art',
+      description: 'AI를 활용해 나만의 러닝 경로 이미지를 생성하고 공유할 수 있는 웹 서비스입니다.',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
